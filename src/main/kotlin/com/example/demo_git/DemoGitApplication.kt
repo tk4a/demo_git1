a0de3a542007b43ff3f5db425def892379ac6145
Ткачёв Артём <atkachev@tsconsulting.com>
2020-12-15 12:45:27 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/demo_git/DemoGitApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demo_git
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoGitApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoGitApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/demo_git/DemoGitApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.demo_git
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoGitApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
